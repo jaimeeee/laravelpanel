a8028389aa7881b7c46ef2f4294a8121a46d3efe
Jaime Zaragoza <hola@jaimeeee.com>
2016-10-05 18:29:59 -0500

    Image field improvements

--- src/LaravelPanel/fields/image/ImageField.php
+++ src/LaravelPanel/fields/image/ImageField.php
@@ -2,6 +2,7 @@
 
 namespace Jaimeeee\Panel\Fields\Image;
 
+use Schema;
 use Jaimeeee\Panel\HTMLBrick;
 use Jaimeeee\Panel\Fields\InputField;
 use Intervention\Image\ImageManagerStatic as Image;
@@ -41,12 +42,48 @@ class ImageField extends InputField
         if ($request->hasFile($field)) {
             $directory = public_path(isset($options['path']) ? $options['path'] : 'storage/images') . '/';
             
-            $filename = $record->id; // TODO: Allow file format changes; ex: {id}_{rand:5}_{timestamp}
+            // Allow file format changes; ex: {id}_{rand:5}_{timestamp}
+            if (isset($options['filename'])) {
+                $filename = $options['filename'];
+                
+                // Replace {id}
+                $filename = str_replace('{id}', $record->id, $filename);
+                
+                // Replace {rand:?}
+                if (preg_match_all('/{rand(:[0-9+]+)*}/', $filename, $matches)) {
+                    foreach ($matches[0] as $match) {
+                        $parts = explode(':', $match);
+                        
+                        $digits = isset($parts[1]) ? intval($parts[1]) : 8;
+                        
+                        $filename = str_replace($match, substr(uniqid(rand()), 0, $digits), $filename);
+                    }
+                }
+                
+                // Replace timestamp
+                $filename = str_replace('{timestamp}', time(), $filename);
+                
+                // Replace original name
+                $fileOriginalName = str_replace($request->file($field)->getClientOriginalExtension(), '',
+                                                $request->file($field)->getClientOriginalName());
+                $filename = str_replace('{original}', str_slug($fileOriginalName, '-'), $filename);
+            } else {
+                $filename = $record->id;
+            }
             
             $originalFilename = $filename . '_o.' . $request->file($field)->guessExtension();
             
             $request->file($field)->move($directory, $originalFilename);
             
+            // If the record requires to save the filename
+            // default: image_name
+            $imageNameColumn = isset($options['imageNameColumn']) ? $options['imageNameColumn'] : 'image_name';
+            if (Schema::hasColumn($record->getTable(), $imageNameColumn)) {
+                $record->$imageNameColumn = $filename;
+                $record->update();
+            }
+            
+            // Update to each size
             if (isset($options['sizes'])) {
                 foreach ($options['sizes'] as $value) {
                     $method = 'resize';
