a582e2809c2d4325bd7368ec010a69f182555b0b
Jaime Zaragoza <hola@jaimeeee.com>
2016-12-22 17:06:47 -0600

    Select field custom options

--- src/LaravelPanel/fields/select/SelectField.php
+++ src/LaravelPanel/fields/select/SelectField.php
@@ -16,21 +16,26 @@ class SelectField extends BaseField
      */
     public function field()
     {
+        $optionsCode = '';
+        
         if (is_array($this->options['options'])) {
-            // TODO: Show custom options, not from a database
+            foreach ($this->options['options'] as $key => $value) {
+                $optionsCode .= '<option value="'.$key.'"'.($this->value == $key ?
+                    ' selected="selected"' : null).'>'.
+                    $value.'</option>';
+            }
         } elseif ($optionClass = $this->options['options']) {
             $options = $optionClass::orderBy((isset($this->options['optionsSort']) ? $this->options['optionsSort'] : 'id'),
                                               (isset($this->options['optionsSortOrder']) ? $this->options['optionsSortOrder'] : 'asc'))->get();
-        }
+            
+            foreach ($options as $option) {
+                $optionValue = isset($this->options['optionsValue']) ? $this->options['optionsValue'] : 'id';
+                $optionShow = isset($this->options['optionsShow']) ? $this->options['optionsShow'] : 'name';
 
-        $optionsCode = '';
-        foreach ($options as $option) {
-            $optionValue = isset($this->options['optionsValue']) ? $this->options['optionsValue'] : 'id';
-            $optionShow = isset($this->options['optionsShow']) ? $this->options['optionsShow'] : 'name';
-
-            $optionsCode .= '<option value="'.$option->$optionValue.'"'.($this->value == $option->$optionValue ?
-                ' selected="selected"' : null).'>'.
-                $option->$optionShow.'</option>';
+                $optionsCode .= '<option value="'.$option->$optionValue.'"'.($this->value == $option->$optionValue ?
+                    ' selected="selected"' : null).'>'.
+                    $option->$optionShow.'</option>';
+            }
         }
 
         $input = new HTMLBrick('select', $optionsCode, [
