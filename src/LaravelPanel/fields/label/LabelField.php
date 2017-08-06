6a7115a95415f1d665f467541d30becfbfc6cc1b
Jaime Zaragoza <hola@jaimeeee.com>
2017-08-06 18:12:24 +0200

    LabelField

---
+++ src/LaravelPanel/fields/label/LabelField.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Jaimeeee\Panel\Fields\Label;
+
+use Jaimeeee\Panel\Fields\BaseField;
+use Jaimeeee\Panel\HTMLBrick;
+
+class LabelField extends BaseField
+{
+    public $placeholder;
+
+    /**
+     * Creates the field code.
+     *
+     * @return HTMLBrick
+     */
+    public function field()
+    {
+        $label = new HTMLBrick('p', $this->label);
+        $label->addClass('form-control-static');
+        return $label;
+    }
+}
