211599ba0be7dc5ea11af292a75d4104c41512ca
Jaime Zaragoza <hola@jaimeeee.com>
2017-08-07 16:19:15 +0200

    Fixed LabelField not being ignored

--- src/LaravelPanel/fields/label/LabelField.php
+++ src/LaravelPanel/fields/label/LabelField.php
@@ -2,12 +2,58 @@
 
 namespace Jaimeeee\Panel\Fields\Label;
 
-use Jaimeeee\Panel\Fields\BaseField;
 use Jaimeeee\Panel\HTMLBrick;
 
-class LabelField extends BaseField
+class LabelField
 {
-    public $placeholder;
+    public static $ignore = true;
+
+    public $label;
+    public $value;
+
+    public function __construct($name, $options)
+    {
+        if (isset($options['label'])) {
+            $this->label = $options['label'];
+        }
+        if (isset($options['value'])) {
+            $this->value = $options['value'];
+        }
+    }
+
+    /**
+     * Show the div container.
+     *
+     * @return string HTML code
+     */
+    public function code()
+    {
+        $fieldWrapper = new HTMLBrick('div', $this->field());
+        $fieldWrapper->addClass('col-lg-9');
+
+        $formGroup = new HTMLBrick('div', $this->label().$fieldWrapper);
+        $formGroup->addClass('form-group');
+
+        return $formGroup;
+    }
+
+    /**
+     * Creates the label code.
+     *
+     * @return HTMLBrick
+     */
+    public function label()
+    {
+        if (!$this->label) {
+            return;
+        }
+
+        $label = new HTMLBrick('label', $this->label);
+        $label->addClass('col-lg-2');
+        $label->addClass('control-label');
+
+        return $label;
+    }
 
     /**
      * Creates the field code.
